efb767080e25fc2d7a117eb1080034ff7a0ca058
smhrd <smhrd@DESKTOP-6QJRU0D>
2021-10-13 17:42:37 +0900

    MulGame 넣기!

---
+++ GameLetgo/src/lwa/MulGame.java
@@ -0,0 +1,32 @@
+package lwa;
+
+import java.util.Random;
+
+public class MulGame implements iGame {
+
+	private Random ran;
+	private int num1;
+	private int num2;
+	
+	@Override
+	public void makeRandom() {
+		ran = new Random();
+		
+		// 1~9까지의 랜덤수를 생성하여 num1과 num2에 초기화
+		num1 = ran.nextInt(9)+1;
+		num2 = ran.nextInt(9)+1;
+		
+	}
+	
+	@Override
+	public String getQuizMsg() {
+		return num1 + " * " + num2 + " = ";
+	}
+	
+	@Override
+	public boolean checkAnswer(int answer) {
+		return (answer == (num1 * num2)) ? true : false;
+	}
+	
+	}
+}
